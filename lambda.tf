e56f77a2a658f199b981fa9f7106383094ffb262
vissu1234 <34542812+vissu1234@users.noreply.github.com>
2025-07-22 21:49:03 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -17,12 +17,50 @@ resource "aws_iam_role" "lambda_exec" {
 
   tags = var.common_tags
 }
-
+###########################################################
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+####################################################################
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.prefix}-lambda-exec-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy" "lambda_exec_inline" {
+  name = "${var.prefix}-lambda-exec-inline-policy"
+  role = aws_iam_role.lambda_exec.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:CreateRole",
+          "iam:PutRolePolicy",
+          "iam:AttachRolePolicy",
+          "iam:PassRole"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+#############################################################################
 # ------------------------------
 # Lambda Function
 # ------------------------------
