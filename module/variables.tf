2326e4336991298006b0870d387800bebcefd3f2
vissu1234 <34542812+vissu1234@users.noreply.github.com>
2025-09-04 22:14:29 +0530

    Update variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -72,7 +72,15 @@ variable "alb_ingress_rules" {
     }
   ]
 }
-
+variable "alb_ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = optional(string)
+  }))
+}
 variable "target_groups" {
   description = "Map of target groups to create"
   type = map(object({
